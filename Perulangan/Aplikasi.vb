d2f2aa2cdd94fed3834e9fe15b4cf5c2adb3054b
CindyJacquelineAlexandraNanlohy <cindy.jacq@yahoo.com>
2015-10-17 13:35:45 +0700

    Tugas-3 Perulangan

---
+++ Perulangan/Aplikasi.vb
@@ -0,0 +1,17 @@
+﻿Public Class Aplikasi
+
+    Private Sub ProblemBintangToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProblemBintangToolStripMenuItem.Click
+        Problem_Bintang.MdiParent = Me
+        Problem_Bintang.Show()
+    End Sub
+
+    Private Sub MatriksKalkulatorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MatriksKalkulatorToolStripMenuItem.Click
+        Matriks_Calculator.MdiParent = Me
+        Matriks_Calculator.Show()
+    End Sub
+
+
+    Private Sub KeluarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KeluarToolStripMenuItem.Click
+        Environment.Exit(0)
+    End Sub
+End Class
